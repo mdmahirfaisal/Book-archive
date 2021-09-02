818d6e2b806f38282c705ae642d05e296b841e02
unknown <rjmahir.faisal@gmail.com>
2021-09-02 17:36:15 +0600

    condition added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,57 @@
 ////////// search input 
-
 const spinner = style => {
     document.getElementById('spinner').style.display = style;
 }
+////////// search field area //////////////
 
 const searchInputField = async () => {
     const searchInput = document.getElementById('input-field');
     const searchText = searchInput.value;
 
-    /// spinner show 
+    ////// spinner show ////////
     spinner('block');
+
     searchInput.value = '';
     const url = ` http://openlibrary.org/search.json?q=${searchText}`;
 
     const res = await fetch(url);
     const data = await res.json();
-    displaySearchResult(data.docs);
+    if (data.numFound === 0) {
+        const parentDiv = document.getElementById('search-result');
+        parentDiv.textContent = '';
+        const notFound = document.getElementById('not-found');
+        notFound.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1 class="text-center text-warning">No Result Found </h1>
+        <h3 class="text-center">Plase search another book </h3>
+        `;
+        notFound.appendChild(div);
+        spinner('none')
+    }
+    else if (data.numFound > 0) {
+        displaySearchResult(data.docs);
+        const notFound = document.getElementById('not-found');
+        notFound.textContent = '';
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h2 class="text-center">No Result Found </H2>
+        `;
+    };
 
+    //// vew total and giv result/////
+    const vewResult = document.getElementById('vew-result');
+    vewResult.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3 class="text-center text-dark">Available Total Result Now:(<span class="text-danger">${data.numFound}</span>). </h3>
+    <h3 class="text-center text-dark">Giv You Result Now:(<span class="text-danger">${data.docs.length} </span>). </h3>
+    `;
+    vewResult.appendChild(div);
 };
-///// display search result
+///// display search result///////
 
 const displaySearchResult = books => {
-    console.log(books.length);
-
-
     const parentDiv = document.getElementById('search-result');
     parentDiv.textContent = '';
 
@@ -39,26 +67,29 @@ const displaySearchResult = books => {
             
             <h5 class="card-title">Title: ${book.title ? book.title : 'No title'} </h5>
 
-            <p class="card-text">Author: ${book.author_name ? book.author_name : 'Not found name'} </p>
+            <p class="card-text">Author: ${book.author_name ? book.author_name : 'Not found name'}. </p>
             
-            <p class="card-text">Publish Date: ${book.publish_date ? book.publish_date : 'Publish date is not show'} </p>
+            <p class="card-text">Publish Date: ${book.publish_date ? book.publish_date : 'Publish date is not show'}. </p>
 
-            <p class="card-text">Publisher:${book.publisher ? book.publisher : 'Publisher details found'} </p>
+            <p class="card-text">Publisher:${book.publisher ? book.publisher : 'Publisher details found'}. </p>
 
-            <p class="card-text">Person: ${book.person ? book.person : 'No detail'} </p>
+            <p class="card-text">Person: ${book.person ? book.person : 'No detail'}. </p>
             
-            <p class="card-text"> </p>
+            <p class="card-text"> Subject:${book.subject ? book.subject : 'No detail'}. </p>
+            
+            <p class="card-text">Place: ${book.place ? book.place : 'No place'} </p>
              
                 <p class="card-text">Publisher: ${book.publisher ? book.publisher : 'no Pbulishing'}.</p>
+
                 <p> ${book._version_ ? book._version_ : 'NO version'}
              </div>
-             <div class="">
-             <small class="text-muted">Last Modified: ${book.last_modified_i ? book.last_modified_i : 'No modified'}</small>
+             <div class="card-footer">
+             <p class="text-muted">Last Modified: ${book.last_modified_i ? book.last_modified_i : 'No modified'}</p>
             </div>
         </div>
         `;
         parentDiv.appendChild(div);
     });
-    // spinner hide 
+    // spinner hide //
     spinner('none')
 };
